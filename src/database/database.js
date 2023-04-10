ede81e2b8db39ac5e223df0c831425c1041b19ea
Tom Johansson <Tom.Johansson@yh.nackademin.se>
2023-04-10 11:16:09 +0200

    Added connection code to db

---
+++ src/database/database.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+//get dotenv
+require("dotenv").config();
+
+async function main() {
+  const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.smsizof.mongodb.net/?retryWrites=true&w=majority`;
+
+  const connection = await MongoClient.connect(url);
+
+  const database = connection.db("test");
+
+  const coll = database.collection("test");
+
+  const documents = await coll.find({}).toArray();
+  console.log(documents);
+}
+main();
