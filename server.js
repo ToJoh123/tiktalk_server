f5b2c5a6a1ca25bd08092f290f52407b0a543165
Tom Johansson <tom.johansson123@gmail.com>
2023-04-17 12:48:29 +0200

    Added local adress to fix local issue

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser'); // Require cookie-parse
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser"); // Require cookie-parse
 const app = express();
 
 // Cors middleware
-app.use(cors({ origin: "http://localhost:5500", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser()); // Add cookie-parser middleware
 
 // Import the database.js file
-const { main } = require('./src/database/database');
-const authRouter = require('./src/routes/authentication_router');
-const verifyToken = require('./src/routes/middlewares/verifyToken');
-const postsRouter = require('./src/routes/posts_router');
-app.use('/', authRouter);
-app.use('/posts', verifyToken, postsRouter);
-
+const { main } = require("./src/database/database");
+const authRouter = require("./src/routes/authentication_router");
+const verifyToken = require("./src/routes/middlewares/verifyToken");
+const postsRouter = require("./src/routes/posts_router");
+app.use("/", authRouter);
+app.use("/posts", verifyToken, postsRouter);
 
 // Call the main() function to establish the database connection.
 main()
   .then(() => {
-    console.log('Database connection established successfully');
+    console.log("Database connection established successfully");
     // Start your server here
     app.listen(3000, () => {
-      console.log('Server is running on http://localhost:3000');
+      console.log("Server is running on http://localhost:3000");
     });
   })
   .catch((error) => {
-    console.error('Failed to establish database connection:', error);
+    console.error("Failed to establish database connection:", error);
   });
 
 module.exports = app; // Export the app instance
