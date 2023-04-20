b627a2fd2a7ab0e48ab030b1d9e57e284e941584
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-04-20 16:06:09 +0200

    Removed duplicated line.

--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { getComments } = require("../controllers/comments/getComments");
 const { postComment } = require("../controllers/comments/postComment");
 const { deleteComment } = require("../controllers/comments/deleteComment");
-const { deleteComment } = require("../controllers/comments/deleteComment");
 const { countFollowers } = require("../controllers/countFollowers");
 
 const commentsRouter = express.Router();
