a3d6e4eec788f9ab619767f8d0838b8e975fb09f
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-04-20 14:41:09 +0200

    Count number of followers/following.

--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { getComments } = require("../controllers/comments/getComments");
 const { postComment } = require("../controllers/comments/postComment");
+const { countFollowers } = require("../controllers/countFollowers");
 
 const commentsRouter = express.Router();
 commentsRouter.get("/", getComments);
 commentsRouter.post("/", postComment);
+commentsRouter.get("/count", countFollowers);
 
 module.exports = commentsRouter;
