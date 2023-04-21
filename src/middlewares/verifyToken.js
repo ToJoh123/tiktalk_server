3e70d0bed9488ecf6498baddc1d49b56a9cad927
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-04-21 09:22:46 +0200

    Check for the jwt in both cookies and headers.

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -3,21 +3,26 @@ const dotenv = require('dotenv');
 dotenv.config();
 const secret = process.env.JWT_SECRET_KEY;
 
-// Middleware to verify JWT token
 const verifyToken = function verifyToken(req, res, next) {
   try {
-    const jwtToken = req.cookies.jwt || req.headers.authorization.split(' ')[1]; // Update this line to check for the token in both cookies and headers
+    let jwtToken = req.cookies.jwt;
+    if (!jwtToken) {
+      const authHeader = req.headers.authorization;
+      if (authHeader && authHeader.startsWith('Bearer ')) {
+        jwtToken = authHeader.split(' ')[1];
+      }
+    }
+
     if (!jwtToken) {
       res.status(401).json('Token not found');
       return;
     }
-    // Verify the token and catch any errors
+
     jwt.verify(jwtToken, secret, (error, loggedInUserToken) => {
       if (error) {
         res.status(401).json('Authentication error: ' + error.message);
         return;
       }
-      // If the token is valid, set the decoded payload to the request object
       req.user = loggedInUserToken;
       next();
     });
