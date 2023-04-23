a0182cb01af208ef31b12c3036b56900e275a5db
Tom Johansson <tom.johansson123@gmail.com>
2023-04-23 12:46:04 +0200

    Changed to .get method

--- src/routes/followRouter.js
+++ src/routes/followRouter.js
@@ -4,9 +4,9 @@ const { getFollows } = require("../controllers/follow/getFollows");
 const { addFollow } = require("../controllers/follow/addFollow");
 
 const followRouter = express.Router();
-//Need to validate the controllers. 
-followRouter.get("/count", countFollowers); 
+//Need to validate the controllers.
+followRouter.get("/count", countFollowers);
 followRouter.get("/follows", getFollows); //Get followers and the following accounts.
-followRouter.post("/add", addFollow); //Get followers and the following accounts.
+followRouter.get("/add", addFollow); //Get followers and the following accounts.
 
 module.exports = followRouter;
