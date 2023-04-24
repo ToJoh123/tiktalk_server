093a33c9a3f7d95e3ef905e79e53b028c0d22313
Tom Johansson <tom.johansson123@gmail.com>
2023-04-24 08:46:24 +0200

    Removed unwanted console.log

--- src/controllers/comments/user/getCurrentUserComments.js
+++ src/controllers/comments/user/getCurrentUserComments.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 
 exports.getCurrentUserComments = async function (req, res) {
   const decoded = jwt.decode(req.cookies.jwt);
-  console.log("decoded", decoded);
   try {
     const data = await db.comments.find({ userId: decoded._id }).toArray();
     res.status(200).json({
