09ba2402a817c7d54d7ee5c3058bbc5ffaca66d9
Tom Johansson <tom.johansson123@gmail.com>
2023-04-24 07:05:50 +0200

    Added function getCurrentUserComments to Router

--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -2,11 +2,14 @@ const express = require("express");
 const { getComments } = require("../controllers/comments/getComments");
 const { postComment } = require("../controllers/comments/postComment");
 const { deleteComment } = require("../controllers/comments/deleteComment");
+const {
+  getCurrentUserComments,
+} = require("../controllers/comments/user/getCurrentUserComments");
 
 const commentsRouter = express.Router();
 commentsRouter.get("/", getComments);
 commentsRouter.post("/", postComment);
 commentsRouter.delete("/", deleteComment);
-
+commentsRouter.get("/user", getCurrentUserComments);
 
 module.exports = commentsRouter;
