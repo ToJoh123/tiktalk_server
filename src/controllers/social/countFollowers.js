18d9ea1952fbb7f65998cd02d6a2551db5f7f027
Jonathan <jonteljungp@gmail.com>
2023-04-26 22:21:01 +0200

    Added validation to countFollowers

--- src/controllers/social/countFollowers.js
+++ src/controllers/social/countFollowers.js
@@ -1,9 +1,18 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const joi = require("joi");
 
 const db = require("../../database/db"); //Connect.
 
+const countFollowersSchema = joi.object({
+  username: joi.string().allow(null, '').min(1).max(150),
+});
+
 const countFollowers = async (req, res) => {
+  const { error } = countFollowersSchema.validate(req.query);
+  if (error) {
+    return res.status(400).json(error.details[0].message);
+  }
   const username = req.query.username || req.user.username; //If username parameter is not there, use loggedInUser value.
   try {
     const followCollection = db.follow;
