d0809f652ebc5b7eff02fb90057db68c37a560b8
Jonathan <jonteljungp@gmail.com>
2023-04-26 22:20:42 +0200

    Moved controller to other dir and route

--- src/routes/authentication_router.js
+++ src/routes/authentication_router.js
@@ -3,7 +3,6 @@ const authRouter = express.Router();
 const { login } = require('../controllers/authentication/loginController');
 const { register } = require('../controllers/authentication/registerController');
 const { logout } = require('../controllers/authentication/logoutController');
-const { getUserInfo } = require('../controllers/getUserController');
 
 //Middleware to parse JSON request bodies.
 authRouter.use(express.json());
@@ -11,6 +10,5 @@ authRouter.use(express.json());
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
-authRouter.get('/api/userinfo', getUserInfo);
 
 module.exports = authRouter;
--- src/routes/socialRouter.js
+++ src/routes/socialRouter.js
@@ -4,6 +4,7 @@ const { getFollows } = require("../controllers/social/getFollows");
 const { addFollow } = require("../controllers/social/addFollow");
 const { getUsers } = require("../controllers/social/getUsers");
 const { getProfileInfo } = require("../controllers/social/getProfileController");
+const { getUserInfo } = require('../controllers/social/getUserController');
 
 const socialRouter = express.Router();
 socialRouter.get("/count", countFollowers); //Count number of follows and following.
@@ -11,5 +12,6 @@ socialRouter.get("/follows", getFollows); //Get the username of followers and th
 socialRouter.post("/add", addFollow); //Follow another account.
 socialRouter.get("/users", getUsers); //Get all users that is registered and render out the accounts you follow and not follow.
 socialRouter.get("/globalProfile", getProfileInfo); //Use a new router for this.
+socialRouter.get('/api/userinfo', getUserInfo);
 
 module.exports = socialRouter;
