4a22369eb55909a62c47d939d0df380ef06abbba
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-04-26 20:39:54 +0200

    Added new parameter to query.

--- src/controllers/social/countFollowers.js
+++ src/controllers/social/countFollowers.js
@@ -4,11 +4,11 @@ dotenv.config();
 const db = require("../../database/db"); //Connect.
 
 const countFollowers = async (req, res) => {
-  const loggedInUser = req.user.username; 
+  const username = req.query.username || req.user.username; //If username parameter is not there, use loggedInUser value.
   try {
-    const followCollection = db.follow; 
-    const followersCount = await followCollection.countDocuments({ username: loggedInUser }); //Followers.
-    const followingCount = await followCollection.countDocuments({ follower: loggedInUser }); //Following.
+    const followCollection = db.follow;
+    const followersCount = await followCollection.countDocuments({ username: username }); //Followers.
+    const followingCount = await followCollection.countDocuments({ follower: username }); //Following.
     res.status(200).json({ followers: followersCount, following: followingCount });
   } catch (err) {
     res.status(500).json('Error connecting to MongoDB');
