71a0b973e207551601a0eeb3b56dbafe03bb8ec7
Tom Johansson <tom.johansson123@gmail.com>
2023-04-27 08:02:15 +0200

    Deleted old connection

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(
 
 app.use(cookieParser()); //Add cookie-parser middleware.
 
-// Import the database.js file
-const { main } = require("./src/database/database");
 const authRouter = require("./src/routes/authentication_router");
 const verifyToken = require("./src/middlewares/verifyToken");
 const commentsRouter = require("./src/routes/commentsRouter");
@@ -27,18 +25,10 @@ app.use("/", authRouter);
 app.use("/comments", verifyToken, commentsRouter);
 app.use("/profile", verifyToken, socialRouter);
 
-//Call the main() function to establish the database connection.
-main()
-  .then(() => {
-    console.log("main():Database connection established successfully");
-    //Start your server here
-    app.listen(3000, () => {
-      console.log("Server is running on http://localhost:3000");
-    });
-  })
-  .catch((error) => {
-    console.error("Failed to establish database connection:", error);
-  });
 //new class to handle connections TODO:Replace main() with this class
-db.connect();
+db.connect().then(() => {
+  app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
+});
 module.exports = app; // Export the app instance
