3ab8fc32d81d96b36b921530ed5ab606550c23a1
Tom Johansson <tom.johansson123@gmail.com>
2023-04-27 09:03:19 +0200

    Changed the db connection file

--- tests/registration.test.js
+++ tests/registration.test.js
@@ -1,77 +1,89 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
-const supertest = require('supertest');
-const app = require('../server');
-const { registerSchema } = require('../src/validation/registerschema')
+const supertest = require("supertest");
+const app = require("../server");
+const { registerSchema } = require("../src/validation/registerschema");
+const db = require("../src/database/db");
 
-const { main } = require('../src/database/database'); 
+// const { main } = require('../src/database/database');
 
-let coll; // Collection variable from database.js
-
-describe('/register', () => {
+describe("/register", () => {
   //Main function to connect.
   before(async () => {
-    coll = await main();
+    await db.connect();
   });
 
-  it('should insert a user when provided with valid credentials', (done) => {
+  it("should insert a user when provided with valid credentials", (done) => {
     supertest(app)
-      .post('/register')
-      .send({ firstname: 'Test', surname: 'Testsson', username: 'jadajada', password: 'jadajada' })
+      .post("/register")
+      .send({
+        firstname: "Test",
+        surname: "Testsson",
+        username: "jadajada",
+        password: "jadajada",
+      })
       .expect(201)
       .end(async (err, res) => {
         expect(res.status).to.equal(201);
-        expect(res.body).to.have.property('message').equal('User registered successfully');
+        expect(res.body)
+          .to.have.property("message")
+          .equal("User registered successfully");
 
         //Check if user was inserted into MongoDB collection.
-        const users = await coll.find({ username: 'jadajada' }).toArray();
+        const users = await db.users.find({ username: "jadajada" }).toArray();
         expect(users.length).to.equal(1);
-        expect(users[0]).to.have.property('username').equal('jadajada');
+        expect(users[0]).to.have.property("username").equal("jadajada");
 
         //Delete the inserted user.
-        await coll.deleteOne({ username: 'jadajada' });
+        await db.users.deleteOne({ username: "jadajada" });
 
         done();
       });
   });
 
-  it('should return error where username is already in use', (done) => {
+  it("should return error where username is already in use", (done) => {
     supertest(app)
-      .post('/register')
-      .send({ firstname: 'Jonathan', surname: 'Ljung', username: 'Jonathan', password: 'password' }) //Replace with invalid credentials.
+      .post("/register")
+      .send({
+        firstname: "Jonathan",
+        surname: "Ljung",
+        username: "Jonathan",
+        password: "password",
+      }) //Replace with invalid credentials.
       .expect(409)
       .end((err, res) => {
         expect(res.status).to.equal(409);
-        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property("message");
         done();
       });
   });
 
-  describe('Joi Validation', () => {
-    it('should validate a correct user object', () => {
+  describe("Joi Validation", () => {
+    it("should validate a correct user object", () => {
       const user = {
-        firstname: 'test',
-        surname: 'testsson',
-        username: 'jadajada',
-        password: 'mypassword123' // Valid password (greater than 6 characters)
+        firstname: "test",
+        surname: "testsson",
+        username: "jadajada",
+        password: "mypassword123", // Valid password (greater than 6 characters)
       };
-  
+
       const { error } = registerSchema.validate(user);
       expect(error).to.be.undefined;
     });
-  
-    it('should return an error for an incorrect user object', () => {
+
+    it("should return an error for an incorrect user object", () => {
       const user = {
-        firstname: 'test',
-        surname: 'testsson',
-        username: 'jadajada',
-        password: 'abc' // Invalid password (less than 6 characters)
+        firstname: "test",
+        surname: "testsson",
+        username: "jadajada",
+        password: "abc", // Invalid password (less than 6 characters)
       };
-  
+
       const { error } = registerSchema.validate(user);
       expect(error).to.not.be.undefined;
-      expect(error.details[0].message).to.equal('Password must be alphanumeric and between 6 and 30 characters long');
+      expect(error.details[0].message).to.equal(
+        "Password must be alphanumeric and between 6 and 30 characters long"
+      );
     });
-  
-})
+  });
 });
